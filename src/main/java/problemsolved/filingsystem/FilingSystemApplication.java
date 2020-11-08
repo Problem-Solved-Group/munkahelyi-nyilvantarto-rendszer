4fc747edda55518aafddd55e5d1e1c034014eb3b
Nagy Viktor <barca0998@gmail.com>
2020-11-08 22:39:05 +0100

    Bug fixed

--- src/main/java/problemsolved/filingsystem/FilingSystemApplication.java
+++ src/main/java/problemsolved/filingsystem/FilingSystemApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilingSystemApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FilingSystemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FilingSystemApplication.class, args);
+    }
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO site(id,name,location) VALUES (1,"HQ","1023,Budapest Tök utca 12.");
